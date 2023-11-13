f76959431085e745703a50a3d43dcad91480c650
danield001 <30894506+danield001@users.noreply.github.com>
2023-11-13 17:37:46 +1030

    changes to js and README.md

--- Develop/README.md
+++ Develop/README.md
@@ -1,14 +1,32 @@
-README.md Generator
-undefined
-## Description
-nn
-## Installation
-nn
-## Usage
-nn
-## Contributions
-nn
-nn
-## Testing
-nn
+# README.md Generator
+<details>
+<summary>Table of Contents</summary>
+<ol>
+  <li><a href="#description">Description</a></li>
+  <li><a href="#installation">Installation</a></li>
+  <li><a href="#usage">Usage</a></li>
+  <li><a href="#contributions">Contributions</a></li>
+  <li><a href="#testing">Testing</a></li>
+  <li><a href="#questions">Questions</a></li>
+  <li><a href="#license">License</a></li>
+</ol>
+</details>
+
+### Description
+The README.md Generator allows for users to easily create a markdown file, which has been formatted for a project README file. By having this, it will allow users to easily create markdown files, through their terminal, by inputting the details in the command line.
+### Installation
+To Install the repository onto your local drive, git clone the repository to your local drive through github.
+### Usage
+To use the project, ensure the repository is cloned onto your local drive, and type node index.js into the terminal. Ensure the path is relative to the repository we are working in. From this, the user will be asked to input the details of their project, in the command line. The code will automatically create a markdown file in the code, and input all the details into the file.
+<video src="./assets/README.md Generator Video.mov" controls="controls" style="max-width: 730px;">
+</video>
+### Contributions
+N/A
+### Testing
+N/A
+## Questions
+GitHub: https://www.github.com/danield001
+Email: danieldaminato@gmail.com
+To ask the developers any questions relating to this project, please contact via email.
 ## License
+This project is licensed under the MIT License.
--- Develop/index.js
+++ Develop/index.js
@@ -1,73 +1,155 @@
 const fs = require('fs');
 const readline = require('readline');
+const inquirer = require('inquirer');
 
 const questions = [
-  'What is the name of your project?',
-  'Please provide a brief description of your project.',
-  'Please provide installation instructions for your project.',
-  'Please provide usage information for your project.',
-  'Please provide contributions for your project.',
-  'Please provide test instructions for your project.',
-  'Please provide the license for your project.',
+    'Please provide a brief description of your project.',
+    'Please provide installation instructions for your project.',
+    'Please provide usage information for your project.',
+    'Please provide contributions for your project.',
+    'Please provide test instructions for your project.',
 ];
 
 const titles = [
-    `${process.argv[2]}`,
-    '## Description',
-    '## Installation',
-    '## Usage',
-    '## Contributions',
-    '## Testing',
-    '## License'
+    '### Description',
+    '### Installation',
+    '### Usage',
+    '### Contributions',
+    '### Testing',
 ];
 
+const fileName = 'README.md';
+
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
-function writeToFile(fileName, data, i) {
-
-if (i === 0) {
-    rl.question(`${questions[i]}\n`, (answer) => {
-        fs.appendFile(fileName, `${titles[i]}\n`, (err) => {
-            if (err) {
-                console.error(err);
-                  } else {
-                  writeToFile(fileName, data, i + 1); // Move to the next question
-             }
-        });
-});
-  else if (i < questions.length) {
-    rl.question(`${questions[i]}\n`, (answer) => {
-        fs.appendFile(fileName, `${titles[i]}\n`, (err) => 
-        err ? console.log(err) : null);
-
-         fs.appendFile(fileName, `${answer}\n`, (err) => {
-            if (err) {
-           console.error(err);
-             } else {
-             writeToFile(fileName, data, i + 1); // Move to the next question
-        }
-      });
-    });
-  }; 
-  else {
-    console.log('README.md file created successfully.');
-    rl.close();
-  }
-}
-}
+const contents =
+    `<details>
+<summary>Table of Contents</summary>
+<ol>
+  <li><a href="#description">Description</a></li>
+  <li><a href="#installation">Installation</a></li>
+  <li><a href="#usage">Usage</a></li>
+  <li><a href="#contributions">Contributions</a></li>
+  <li><a href="#testing">Testing</a></li>
+  <li><a href="#questions">Questions</a></li>
+  <li><a href="#license">License</a></li>
+</ol>
+</details>
+`
 
 function init() {
-  const fileName = 'README.md';
-  fs.writeFile(fileName, '', (err) => {
-    if (err) {
-      console.error(err);
+    fs.writeFile(fileName, '', (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+
+            console.log(`Please name your project`);
+            rl.question('Project Name: ', (answer) => {
+                fs.appendFile(fileName, `# ${answer}\n`, (err) => {
+                    if (err) throw err;
+
+                    fs.appendFile(fileName, `${contents}\n`, (err) => {
+                        if (err) throw err
+                        writeToFile(fileName, '', 0);
+                    });
+                });
+            })
+
+        };
+    });
+}
+function writeToFile(fileName, data, i) {
+    if (i < questions.length) {
+        console.log(`${questions[i]}`);
+        rl.question('Your answer: ', (answer) => {
+            fs.appendFile(fileName, `${titles[i]}\n${answer}\n`, (err) => {
+                if (err) throw err;
+                // Move to the next question
+                writeToFile(fileName, '', i + 1);
+            });
+        });
     } else {
-      writeToFile(fileName, '', 0);
+        // All questions answered, move to the GitHub username input
+        writeGithub();
     }
-  });
+}
+
+function writeGithub() {
+    fs.appendFile(fileName, '## Questions\n', (err) => {
+        if (err) throw err;
+        console.log('Please enter your GitHub username:');
+        rl.question('Your User: ', (githubAnswer) => {
+            fs.appendFile(
+                fileName,
+                `GitHub: https://www.github.com/${githubAnswer}\n`,
+                (err) => {
+                    if (err) throw err;
+                    // Move to the email input
+                    writeEmail();
+                }
+            );
+        });
+    });
+}
+
+function writeEmail() {
+    console.log('Please enter your email:');
+    rl.question('Your Email: ', (emailAnswer) => {
+        fs.appendFile(fileName, `Email: ${emailAnswer}\n`, (err) => {
+            if (err) throw err;
+            // Close the readline interface after collecting both GitHub username and email
+            rl.close();
+            fs.appendFile(fileName, `To ask the developers any questions relating to this project, please contact via email.\n`, (err) => {
+                if (err) throw err;
+                selectLicense();
+            })
+            // Move to selecting the license
+        });
+    });
+}
+
+function selectLicense() {
+    fs.appendFile(fileName, '## License\n', (err) => {
+        if (err) throw err;
+        licenseChoices();
+    });
+}
+
+function licenseChoices() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What kind of license would you like?',
+                choices: ['MIT', 'GPLv3'],
+                name: 'license',
+            },
+        ])
+        .then((response) => {
+            switch (response.license) {
+                case 'MIT':
+                    fs.appendFile(
+                        fileName,
+                        'This project is licensed under the MIT License.\n',
+                        (err) => {
+                            if (err) throw err;
+                        }
+                    );
+                    break;
+                case 'GPLv3':
+                    fs.appendFile(
+                        fileName,
+                        'This project is licensed under the GNU GPL v3.\n',
+                        (err) => {
+                            if (err) throw err;
+                        }
+                    );
+                    break;
+            }
+        });
 }
 
 // Function call to initialize app
